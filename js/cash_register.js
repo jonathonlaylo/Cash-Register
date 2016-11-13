485fe7bfa6465fa948b9b3036a83040d08687b36
Edward Gao <egao@sent.com>
2016-11-12 22:56:05 -1000

    RESTART number buttons

--- js/cash_register.js
+++ js/cash_register.js
@@ -24,100 +24,45 @@ var withdraw = document.getElementById('withdraw');
 var equal  = document.getElementById('equal');
 
 cashRegister = (function() {
-  //declare variables
-  var _tempNum = 0;
+
+  var _decTracker = 0;
   var _decMode = false;
-  var _decPlace = 1;
-  var _trackDecZeros = 0; // zero tracker to fix last number zero no display bug
 
-  // display currently active number
-  function _showNum() {
-    display.innerText = "$" + _tempNum;
-  }
-
-  // generic function for numerical button presses
   function _pressNumButton(button) {
-    if(_decMode === false) { // non decimal mode
-      _tempNum *= 10;
-      _tempNum += parseInt(button.innerText);
-      _showNum();
-    } else { // decimal mode
-      if(_decPlace > 0.01) { //if smaller than hundredth decimal digit
-        _decPlace /= 10;
-        _tempNum = _tempNum + _decPlace * parseInt(button.innerText);
-        _showNum();
-        // because it is making decimals like 5.1099999999999
-        _fixDecimalZeros(button);
-      }else{ //if entering thousandth decimal digit territory
-        message.innerText = "The smallest US currency denomination is $0.01";
+    if(display.innerText === "0") {
+      display.innerText = "";
+    } // clear initial zero
+    if(_decTracker < 2) { // check if less than 2 decimal digits
+      if(_decTracker === 1 && button.innerText === "00") {
+        display.innerText += "0"; // fix three digits at decimal when "00" is pressed bug
+      }else{
+        display.innerText += button.innerText; // concat str
+        if(display.innerText === "00") {
+          display.innerText = 0;
+        }
       }
+      _decActions(button); // perform decimal actions
+    }else{
+      message.innerText = "$0.01 is the smallest US currency denomination"; // error
     }
   }
 
-  // special function for double zero button
-  function _pressDoubleZero() {
-    if(_decMode === false) {
-      _tempNum *= 100;
-      _showNum();
-    } else {
-      if(_decPlace === 0.01) {
-        message.innerText = "The smallest US currency denomination is $0.01";
-      }
-      if(_decPlace >= 0.01) {
-        _decPlace /= 100;
-        display.innerText = "$" + _tempNum + ".00";
-      }
-    }
-  }
-
-  // because it is making decimals like 5.1099999999999
-  function _cutTenth(num) {
-    var newNum = Math.round(num * 10) / 10;
-    return newNum;
-  }
-
-  // because it is making decimals like 5.1099999999999
-  function _cutHundredth(num) {
-    var newNum = Math.round(num * 100) / 100;
-    return newNum;
-  }
-
-  // fix decimal artifacts and zeros in decimal places
-  function _fixDecimalZeros(button) {
-    if(_decPlace === 0.1) {
-      _tempNum = _cutTenth(_tempNum);
-      // fix first zero in decimal
-      if(parseInt(button.innerText) === 0) {
-        _trackDecZeros += 1;
-        display.innerText = "$" + _tempNum + ".0";
-      }
-    }else if(_decPlace === 0.01) {
-      _tempNum = _cutHundredth(_tempNum);
-      // fix zero after a non-zero number in decimal
-      if(parseInt(button.innerText) === 0) {
-        display.innerText = "$" + _tempNum + "0";
-      }
-      // fix second zero in a row in decimal
-      if(parseInt(button.innerText) === 0 && _trackDecZeros === 1) {
-        display.innerText = "$" + _tempNum + ".00";
-      }
-    }
-  }
-
-  // activate decimal mode
-  function _actDec() {
-    _decMode = true;
+  function _decActions(button) {
+    if(_decMode) {
+      _decTracker++;
+    } // increment _decTracker if _decMode on and button not "."
+    if(button.innerText === ".") {
+      _decMode = true;
+    } // turn on _decTracker if button is "."
+    if(_decMode && button.innerText === "00") {
+      _decTracker ++;
+    } // increment _decTracker again if button is "00"
   }
 
   return {
-    showNum: _showNum,
-    pressNumButton: _pressNumButton,
-    pressDoubleZero: _pressDoubleZero,
-    actDec: _actDec
+    pressNumButton: _pressNumButton
   };
 })();
-
-var myCashRegister = cashRegister;
 
 equal.addEventListener('click', function(){cashRegister.showNum();});
 one.addEventListener('click', function(){cashRegister.pressNumButton(one);});
@@ -130,5 +75,5 @@ seven.addEventListener('click', function(){cashRegister.pressNumButton(seven);})
 eight.addEventListener('click', function(){cashRegister.pressNumButton(eight);});
 nine.addEventListener('click', function(){cashRegister.pressNumButton(nine);});
 zero.addEventListener('click', function(){cashRegister.pressNumButton(zero);});
-doubleZero.addEventListener('click', function(){cashRegister.pressDoubleZero();});
-decimal.addEventListener('click', function(){cashRegister.actDec();});
+doubleZero.addEventListener('click', function(){cashRegister.pressNumButton(doubleZero);});
+decimal.addEventListener('click', function(){cashRegister.pressNumButton(decimal);});
